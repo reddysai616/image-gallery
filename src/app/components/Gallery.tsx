c8daca2da9992c2c15e1bacfacba509bf67047ed
saikiran0701 <62210706+reddysai616@users.noreply.github.com>
2023-10-01 21:13:24 +0530

    img grid

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -3,7 +3,6 @@ import fetchImages from '@/lib/fetchImages'
 import type { imageResults } from '@/models/Images'
 import ImgContainer from './ImgContainer'
 import addBlurredDataUrls from '@/lib/base64generator'
-import Navbar from './Navbar'
 
 type Props={
   topic?:string|undefined
@@ -18,9 +17,8 @@ const images:imageResults|undefined = await fetchImages(url)
 if(!images) return  <h2 className='font-bold mx-0 p-0'>No images Found</h2>
 const photosWithBlur = await addBlurredDataUrls(images)
   return (
-    <>
-  <Navbar/>
-    <section className="px-2 my-3 grid gap-8 grid-cols-gallery">
+  
+    <section className="px-1 my-3 grid grid-cols-gallery auto-rows-[10px]">
       
         <ul>
             {photosWithBlur.map(photo => (
@@ -32,7 +30,7 @@ const photosWithBlur = await addBlurredDataUrls(images)
             
         </ul>
     </section>
-    </>
+    
 
   )
 }
--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { photo } from '@/models/Images';
 import Image from "next/image"
+import Link from 'next/link';
 
 type props={
   Photo:photo
@@ -13,19 +14,27 @@ export default function ImgContainer({Photo}:props) {
   const photoSpans = Math.ceil(galleryHeight / 10)+1
   return (
     <>
-    <div  className='h-64 bg-gray-200 rounded-xl relative overflow-hidden group'>
+    <div  className=' w-[250px] justify-self-center grid   ' style={{
+      gridRow:`span ${photoSpans}`
+    }}>
+      <Link href={Photo.url} target='_blank' className='grid place-content-center'>
+      <div className='rounded-xl overflow-hidden group'>
+
       <Image
       src={Photo.src.large}
       alt={Photo.alt}
-      // width={250}
-      // height={250}
-      fill={true}
+      width={Photo.width}
+      height={Photo.height}
+      // fill={true}
       placeholder='blur'
       blurDataURL={Photo.blurredDataUrl}
-      sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
-      className="object-cover group-hover:opacity-75"
+      // sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+      sizes='250px'
+      className=" group-hover:opacity-75"
 
       />
+      </div>
+      </Link>
 
     </div>
     
